dab1e05322825366f7f878b3d9299a9241a3c1ef
RKarkee <rajankarkee01@gmail.com>
2022-03-30 21:51:01 +0545

    post  update

--- src/Posts/Post.js
+++ src/Posts/Post.js
@@ -1,33 +1,36 @@
-import { Avatar } from '@material-ui/core'
-import React from 'react'
-import './Post.css'
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
-import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
-import ShareIcon from '@material-ui/icons/Share';
-import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
-import InputOption from '../Feeds/InputOption';
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Post.css";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ChatOutlinedIcon from "@material-ui/icons/ChatOutlined";
+import ShareIcon from "@material-ui/icons/Share";
+import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
+import InputOption from "../Feeds/InputOption";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
 
 function Post({ name, description, message, photoUrl }) {
+  // user = useSelector(selectUser);
   return (
-    <div className='post'>
+    <div className="post">
       <div className="post__header">
-          <Avatar />
-          <div className="post__info">
-              <h2>{name}</h2>
-              <p>{description}</p>
-          </div>
+        <Avatar src={photoUrl}>{name[0]}</Avatar>
+        <div className="post__info">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
       </div>
       <div className="post__body">
-          <p>{message}</p>
+        <p>{message}</p>
       </div>
       <div className="post__buttons">
-      <InputOption Icon={ThumbUpAltIcon} title="Like" color="grey" />
-          <InputOption Icon={ChatOutlinedIcon} title="Comment" color="grey" />
-          <InputOption Icon={ShareIcon} title="Share" color="grey" />
-          <InputOption Icon={SendOutlinedIcon} title="Send" color="grey" />
+        <InputOption Icon={ThumbUpAltIcon} title="Like" color="grey" />
+        <InputOption Icon={ChatOutlinedIcon} title="Comment" color="grey" />
+        <InputOption Icon={ShareIcon} title="Share" color="grey" />
+        <InputOption Icon={SendOutlinedIcon} title="Send" color="grey" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Post
+export default Post;
