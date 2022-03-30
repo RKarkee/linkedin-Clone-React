2d02b7e779e148fb78cbb3b0cd3626302aa6b53e
RKarkee <rajankarkee01@gmail.com>
2022-03-30 13:54:13 +0545

    register auth update

--- src/account/Login.js
+++ src/account/Login.js
@@ -1,26 +1,84 @@
-import React from 'react'
-import './Login.css'
+import React, { useState, useEffect } from "react";
+import "./Login.css";
+import { auth } from "../Firebase";
+import { useDispatch } from "react-redux";
+import { login } from "../features/userSlice";
 function Login() {
-    const register =()=>{};
-    const loginToApp=()=>{};
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const dispatch = useDispatch();
+  const register = () => {
+    if (!name) {
+      return alert("Please enter a full name!");
+    }
+    auth.createUserWithEmailAndPassword(email, password).then((userAuth) => {
+      userAuth.user
+        .updateProfile({
+          displayName: name,
+          photoURL: profilePic,
+        })
+        .then(() => {
+          dispatch(
+            login({
+              email: userAuth.user.email,
+              uid: userAuth.user.uid,
+              displayName: name,
+              photoUrl: profilePic,
+            })
+          );
+        });
+    })
+    .catch((error)=>alert(error))
+  };
+  const loginToApp = (e) => {
+    e.preventdefault();
+  };
 
   return (
-    <div className='login'>
-      <img  src="https://logos-world.net/wp-content/uploads/2020/04/Linkedin-Logo-2011-2019.png" alt="" />
-    <form >
-        <input placeholder="Full name(required if registering)" 
-        type="text" />
-        <input placeholder='Profile pic URL(optional)' type="text" />
-        <input placeholder="Email" type="email" />
-        <input placeholder='Password' type="password" />
-        <button type='submit' onClick={loginToApp}>Sign IN</button>
-    </form>
-    <p>Not a member?{" "}
-        <span className='login__register' onClick={register}>Register Now</span>
-    </p>
+    <div className="login">
+      <img
+        src="https://logos-world.net/wp-content/uploads/2020/04/Linkedin-Logo-2011-2019.png"
+        alt=""
+      />
+      <form>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Full name(required if registering)"
+          type="text"
+        />
+        <input
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
+          placeholder="Profile pic URL (optional)"
+          type="text"
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          type="email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          type="password"
+        />
+        <button type="submit" onClick={loginToApp}>
+          Sign IN
+        </button>
+      </form>
+      <p>
+        Not a member?{" "}
+        <span className="login__register" onClick={register}>
+          Register Now
+        </span>
+      </p>
     </div>
-
-  )
+  );
 }
 
 export default Login;
